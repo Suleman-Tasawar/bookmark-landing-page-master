9aa55558064a44735942d74c564ed23db535b61f
Suleman-Tasawar <sulemantasawar9@gmail.com>
2023-08-18 17:36:52 -0700

    Updated the ReadMe

--- index.js
+++ index.js
@@ -54,16 +54,6 @@ for (let i = 0; i < 5; i++) {
   faqToggleBtn[i].addEventListener("click", () => {
     if (faqToggle[i].style.display == "block") {
       faqToggle[i].style.display = "none";
-      faqToggleBtn[i].innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
-        <path
-            fill="none"
-            stroke="#5267DF"
-            stroke-width="3"
-            d="M1 1l8 8 8-8"
-            />
-      </svg>
-      `;
     } else {
       faqToggle[i].style.display = "block";
       faqToggleBtn[i].innerHTML = `
